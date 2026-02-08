4eddbb46e32a0b82333d3cc3ddf21ae15e346672
Priyankaadev <priyaaswal79@gmail.com>
2026-02-08 12:04:17 +0530

    add: images in blog section

--- src/data/blogs.js
+++ src/data/blogs.js
@@ -10,14 +10,14 @@ export const blogs = [
     slug: "blog2",
     title: "Emotional Intelligence Explained",
     date: "Feb 5, 2026",
-     img: "/mind.jpg",
+     img: "/ei.jpg",
     content: `Emotional intelligence is the ability to recognize and manage your emotions.\nIt also involves understanding the emotions of others.\n\nHigh emotional intelligence leads to better relationships and success in life.`
   },
   {
     slug: "blog3",
     title: "Managing Stress Effectively",
     date: "Feb 3, 2026",
-     img: "/mind.jpg",
+     img: "/managestress.jpg",
     content: `Stress is part of life, but it can be managed effectively.\nHere are techniques like deep breathing, meditation, and time management.\n\nRegular exercise and mindfulness also help reduce stress over time.`
   },
 ];
